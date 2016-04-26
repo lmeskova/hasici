b7f7b5b22f4a4d4fd0e245a96b759a29e9857b5f
lmeskova <meskovalu@gmail.com>
2016-04-26 23:15:27 +0200

    All tables 1-34 filled.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,6 +16,42 @@ class DatabaseSeeder extends Seeder
 
         $this->call(InsuranceCompaniesSeeder::class);
         $this->call(FireExtinguishersSeeder::class);
+        $this->call(InjuryCategoriesSeeder::class);
+        $this->call(InjuryCircumstancesSeeder::class);
+        $this->call(InjuryCircumstancesSeeder::class);
+        $this->call(InjuryCategoriesSeeder::class);
+        $this->call(InjuryCausesSeeder::class);
+        $this->call(InjuryTypesSeeder::class);
+        $this->call(OwnershipsSeeder::class);
+        $this->call(DamageSpecificationsSeeder::class);
+        $this->call(DamageTypesSeeder::class);
+        $this->call(IndustryTypesSeeder::class);
+        $this->call(AreasSeeder::class);
+        $this->call(DamageDegreesSeeder::class);
+        $this->call(FireLocationsSeeder::class);
+        $this->call(VehiclePartsSeeder::class);
+        $this->call(ConveyorEquipmentsSeeder::class);
+        $this->call(SegmentFunctionsSeeder::class);
+        $this->call(SegmentAltitudesSeeder::class);
+        $this->call(FireResistancesSeeder::class);
+        $this->call(FlammabilityOfObjectsSeeder::class);
+        $this->call(FireShuttersSeeder::class);
+        $this->call(SpreadingCausesSeeder::class);
+        $this->call(ShutterResistancesSeeder::class);
+        $this->call(FireAlarmsSeeder::class);
+        $this->call(ChemicalsSeeder::class);
+        $this->call(IncidentCausesSeeder::class);
+        $this->call(FlammableSubstancesSeeder::class);
+        $this->call(InitiatorsSeeder::class);
+        $this->call(ElectricalWiringsSeeder::class);
+        $this->call(InitiatorsImpactsSeeder::class);
+        $this->call(BurningSubstancesSeeder::class);
+        $this->call(FollowingSubstancesSeeder::class);
+        $this->call(HazardClassesSeeder::class);
+        $this->call(OrganizationShortcomingsSeeder::class);
+        $this->call(ActionShortcomingsSeeder::class);
+        $this->call(IncidentConclusionsSeeder::class);
+        
         
     }
 }
